8b6a0734713ba900b896dea9495686d049a39348
jumi <ajsel422@gmail.com>
2024-07-29 11:08:45 +0900

    모델 정의

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): # 장고에서 쓰는 규칙 models.Model 상속
+    title = models.CharField(max_length=100) 
+    # CharField 문자형 데이터 (짧은 글자), TextField 문자형 데이터(긴 글자)
+    content = models.TextField()
