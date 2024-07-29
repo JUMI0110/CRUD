d4ebc7aaef049a11126106c90267dc1d09ea6511
jumi <ajsel422@gmail.com>
2024-07-29 12:09:00 +0900

    Read(1) 기능구현

--- posts/views.py
+++ posts/views.py
@@ -11,3 +11,12 @@ def index(request):
     }
     
     return render(request, 'index.html', context)
+
+def detail(request, id):
+    post = Post.objects.get(id=id) # get(컬럼명=value)
+
+    context = {
+        'post': post
+    }
+    
+    return render(request, 'detail.html', context)
